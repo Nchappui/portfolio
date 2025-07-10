8b719d4058c6b8f2065107cbd35f2307424f4196
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-07-10 15:29:51 +0200

    Add Intro component with styling for initial presentation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navigation from "./components/Navigation";
 import AboutMe from "./pages/AboutMe";
 import MyProjects from "./pages/MyProjects";
 import MyExperiences from "./pages/MyExperiences";
+import Intro from "./pages/Intro";
 
 function AppContent() {
   const location = useLocation();
@@ -26,7 +27,15 @@ function AppContent() {
       <Navigation activeTab={getActiveTab()} />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<AboutMe />} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Intro />
+                <AboutMe />
+              </div>
+            }
+          />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/projects" element={<MyProjects />} />
           <Route path="/experiences" element={<MyExperiences />} />
---
+++ src/pages/Intro.js
@@ -0,0 +1,18 @@
+import "./Intro.css";
+
+function Intro() {
+  return (
+    <section className="intro">
+      <div className="intro-container">
+        <div className="name">
+          <p>Nolan Chappuis</p>
+        </div>
+        <div className="job-title">
+          <p>FullStack Developer</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
