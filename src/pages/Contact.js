5902303a5d2e8c4dffd3451d75e896594ef73a49
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-08-12 21:00:00 +0200

    Add SweetAlert2 for form submission feedback in Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,38 @@
 import React from "react";
 import "./Contact.css";
 import Footer from "../components/Footer";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "3581928a-3c1c-4ef8-bb90-cde29dbf89c7");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Your message has been sent successfully!",
+      });
+    } else {
+      console.log("Error", res);
+    }
+  };
+
   return (
     <div className="contact-page">
       <main className="contactContainer">
@@ -11,7 +41,7 @@ const Contact = () => {
             <h2>Feel free to contact me !</h2>
           </div>
 
-          <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
+          <form className="contact-form" onSubmit={onSubmit}>
             <div>
               <label htmlFor="firstName">First name</label>
               <input
